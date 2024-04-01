71bd6fe87759112c6536138d1b5bdf5efd52fb9f
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-04-01 16:22:53 +0800

    Feat: Added index for SDK

---
+++ sdk/src/index.ts
@@ -0,0 +1,11 @@
+import { Base } from "./base";
+import { Posts } from "./posts";
+import { applyMixins } from "./utils";
+
+class AidEyeSDK extends Base {}
+
+interface AidEyeSDK extends Posts {}
+
+applyMixins(AidEyeSDK, [Posts]);
+
+export default AidEyeSDK;
