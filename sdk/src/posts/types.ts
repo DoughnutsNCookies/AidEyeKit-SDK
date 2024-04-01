68e8857feeb2675523a70faaebb072253daba1d6
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-04-01 15:50:23 +0800

    Feat: Posts class done

---
+++ sdk/src/posts/types.ts
@@ -0,0 +1,12 @@
+export declare type Post = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+export declare type NewPost = {
+  userId: number;
+  title: string;
+  body: string;
+};
