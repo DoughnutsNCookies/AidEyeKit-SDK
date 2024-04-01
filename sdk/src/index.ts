3848aa915b6268c0c8edcb4a6cae0b5e44ca6677
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-04-01 16:25:09 +0800

    Style: Removed extra newline

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -3,7 +3,6 @@ import { Posts } from "./posts";
 import { applyMixins } from "./utils";
 
 class AidEyeSDK extends Base {}
-
 interface AidEyeSDK extends Posts {}
 
 applyMixins(AidEyeSDK, [Posts]);
