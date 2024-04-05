005bde2994d1880a97820a314799a1ade46bcdb8
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2024-04-05 06:08:42 +0000

    Chore: Reformat code in openai utils

--- server/src/utils/openai.ts
+++ server/src/utils/openai.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { RED, RESET } from './colors';
-import readline from "readline";
+import readline from 'readline';
 
 /**
  * Converts an image file to a base64 data URI.
@@ -9,18 +9,18 @@ import readline from "readline";
  * @returns {Promise<string>} A promise that resolves with the base64 data URI of the image.
  */
 export const imageToBase64 = async (imageFile: string): Promise<string> => {
-    return await new Promise((resolve, reject) => {
-        fs.readFile(imageFile, (err, data) => {
-            if (err) {
-                console.error(RED + 'Error reading file:' + err + RESET);
-                reject(err);
-                return;
-            }
-            const base64Data = data.toString('base64');
-            const dataURI = `data:image/jpeg;base64,${base64Data}`;
-            resolve(dataURI);
-        });
+  return await new Promise((resolve, reject) => {
+    fs.readFile(imageFile, (err, data) => {
+      if (err) {
+        console.error(RED + 'Error reading file:' + err + RESET);
+        reject(err);
+        return;
+      }
+      const base64Data = data.toString('base64');
+      const dataURI = `data:image/jpeg;base64,${base64Data}`;
+      resolve(dataURI);
     });
+  });
 };
 
 /**
