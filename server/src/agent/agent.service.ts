b12c979be1c15961afd582e83b379b06c4c1b359
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2024-04-05 10:04:11 +0000

    Feat: Added Agent Init function

--- server/src/agent/agent.service.ts
+++ server/src/agent/agent.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { log } from 'src/utils/logging';
-import { RED, BLUE, GREEN, RESET } from 'src/utils/colors';
 import * as fs from 'fs';
-import { getCurrentTime } from 'src/utils/time';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { AgentDTO } from 'src/dto/agent.dto';
+import { agentSystem } from 'src/dto/openai.dto';
+import { MyLogger } from 'src/utils/logging';
+
+const DEBUG = false; // Sets puppeteer to show browser window
 
 @Injectable()
 export class AgentService {
-  currentTime: string;
+  private readonly logger = new MyLogger(AgentService.name);
   imgFilePath: string;
-  logFilePath: string;
 
+  /**
+   * Creates an instance of the AgentService class.
+   * The constructor initializes the current time, sets up directories for saving screenshots and logs,
+   * and creates an empty log file.
+   */
   constructor() {
-    this.currentTime = getCurrentTime();
-
     // Directory to save screenshots
     const imgDir = 'images/';
     if (!fs.existsSync(imgDir)) {
@@ -20,14 +26,26 @@ export class AgentService {
     }
     this.imgFilePath = `${imgDir}screenshot.jpg`;
 
-    // Using date and time in the log file name
-    const logDir = 'logs/';
-    if (!fs.existsSync(logDir)) {
-      fs.mkdirSync(logDir);
-    }
-    this.logFilePath = `${logDir}log-${this.currentTime}.txt`;
-    fs.writeFileSync(this.logFilePath, '');
-
-    log(this.logFilePath, GREEN + 'Logs ready!' + RESET);
+    this.logger.success('Images folder ready!');
   }
+
+  /**
+   * Initializes the agent.
+   *
+   * @returns {Promise<Object>} An object containing the initialized page, browser, messages, url, and screenshotTaken properties.
+   */
+  agentInit = async (): Promise<AgentDTO> => {
+    this.logger.begin('Initializing agent...');
+
+    puppeteer.use(StealthPlugin());
+    const browser = await puppeteer.launch({
+      headless: !DEBUG,
+    });
+
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 1200, deviceScaleFactor: 1 });
+
+    this.logger.success('Agent initialized successfully!');
+    return new AgentDTO(page, browser, [agentSystem], null, false);
+  };
 }
