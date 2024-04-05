cd9657b1b47ca0f18fdaa035baf7cffc63bd4e28
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2024-04-05 06:09:00 +0000

    Feat: Added logging utils

---
+++ server/src/utils/logging.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import { RED, RESET } from './colors';
+
+/* Logs a message to a log file and console.
+ *
+ * @param {string} logfile - The path to the log file.
+ * @param {string} message - The message to be logged.
+ * @returns {Promise<void>} - A promise that resolves when the message is logged.
+ */
+export const log = async (logfile: string, message: string) => {
+  const rawMessage = message.replace(/\x1b\[\d{1,2}m/g, '');
+
+  fs.appendFile(logfile, rawMessage + '\n', (err) => {
+    if (err) {
+      console.error(RED + 'Error writing to log file:' + err + RESET);
+    }
+  });
+  console.log(message);
+};
