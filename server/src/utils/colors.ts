cff915f187820e6af348cddda8f236ce8b9cb296
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2024-04-05 06:04:33 +0000

    Feat: Added color file

---
+++ server/src/utils/colors.ts
@@ -0,0 +1,6 @@
+export const RED = '\x1b[31m';
+export const RESET = '\x1b[0m';
+export const GREEN = "\x1b[32m";
+export const YELLOW = "\x1b[33m";
+export const BLUE = "\x1b[34m";
+export const PURPLE = "\x1b[35m";
