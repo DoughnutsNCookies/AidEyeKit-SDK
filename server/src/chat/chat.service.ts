19f3e05806f37611d945f72113a4efb297cc7f86
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2024-04-06 06:25:06 +0000

    Feat: Hooked agent to chat

--- server/src/chat/chat.service.ts
+++ server/src/chat/chat.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { AgentService } from 'src/agent/agent.service';
 import { MyLogger } from 'src/utils/logging';
+import { Server } from 'socket.io';
 
 @Injectable()
 export class ChatService {
   private readonly logger = new MyLogger(ChatService.name);
 
-  userConnect(client: any): void {
+  constructor(private readonly agentService: AgentService) {}
+
+  userConnect(server: Server, client: any): void {
     this.logger.begin('User Connected');
+    client.join('chat');
+    this.logger.agent('How can I assist you today?');
+    server.to('chat').emit('chat', 'Agent: How can I assist you today?');
+  }
+
+  handleMessage(server: Server, client: any, message: string): void {
+    this.agentService.agent(server, message);
   }
 }
