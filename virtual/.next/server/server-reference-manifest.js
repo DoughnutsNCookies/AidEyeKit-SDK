08f6b23ad86ae0963866020c4ef4a0ecd479cc20
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2024-04-06 18:00:12 +0000

    Chore: Clean up virtual

---
+++ virtual/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"As47rt/XlBtlJ5Xc7sWP0QJkLDU8J+edDXfAW/ZLwAU=\"\n}"
--- virtual/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    VIRTUAL_KEY: process.env.VIRTUAL_KEY,
-    VIRTUAL_PRIVATE: process.env.VIRTUAL_PRIVATE,
-  },
-};
