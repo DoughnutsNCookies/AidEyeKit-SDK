d942898a17028c620d7e293d3537622d070204d8
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2024-04-06 06:24:18 +0000

    Feat: Subscribe to chat using socket

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -1,23 +1,24 @@
 import {
   OnGatewayConnection,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { ChatService } from './chat.service';
-import { AgentService } from 'src/agent/agent.service';
 
 @WebSocketGateway({ namespace: '/chat' })
 export class ChatGateway implements OnGatewayConnection {
-  constructor(
-    private readonly chatService: ChatService,
-    private readonly agentService: AgentService,
-  ) {}
+  constructor(private readonly chatService: ChatService) {}
 
   @WebSocketServer() server: Server;
 
   async handleConnection(client: any, ...args: any[]) {
-    this.chatService.userConnect(client);
-    this.agentService.agent();
+    this.chatService.userConnect(this.server, client);
+  }
+
+  @SubscribeMessage('chat')
+  handleMessage(client: any, message: string) {
+    this.chatService.handleMessage(this.server, client, message);
   }
 }
