df2569a938767ec59950a9e8295ecca0e70b5d2f
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-04-16 18:12:12 +0800

    Feat: Added brief demo

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -1,13 +1,82 @@
+"use client";
 import Image from "next/image";
+import { driver } from "driver.js";
+import "driver.js/dist/driver.css";
 
 export default function Home() {
+  const guide = () => {
+    const driverObj = driver({
+      steps: [
+        {
+          popover: {
+            title: "Welcome!",
+            description: "This is a demo of the AidEyeKit on the NextJS template page",
+          },
+        },
+        {
+          element: "#nextjs",
+          popover: {
+            title: "NextJS",
+            description: "NextJS is a React framework that enables server-side rendering and static site generation",
+          }
+        },
+        {
+          element: "#vercel",
+          popover: {
+            title: "Vercel",
+            description: "Vercel is the platform for Next.js. You can click on this to learn more about Vercel",
+          }
+        },
+        {
+          element: "#docs",
+          popover: {
+            title: "Docs",
+            description: "Click here to find in-depth information about Next.js features and API",
+          }
+        },
+        {
+          element: "#learn",
+          popover: {
+            title: "Learn",
+            description: "Click here to learn about Next.js in an interactive course with quizzes",
+          }
+        },
+        {
+          element: "#template",
+          popover: {
+            title: "Templates",
+            description: "Click here to explore starter templates for Next.js",
+          }
+        },
+        {
+          element: "#deploy",
+          popover: {
+            title: "Deploy",
+            description: "Click here to instantly deploy your Next.js site to a shareable URL with Vercel",
+          }
+        },
+        {
+          element: "#guide",
+          popover: {
+            title: "That's it!",
+            description: "You can click this button to restart the demo. Enjoy!",
+          }
+        }
+      ],
+    });
+    driverObj.drive();
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
+        <button
+          onClick={guide}
+          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+          id="guide"
+        >
+          Click me for a demo!
+        </button>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -17,6 +86,7 @@ export default function Home() {
           >
             By{" "}
             <Image
+              id="vercel"
               src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
@@ -28,7 +98,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div id="nextjs" className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -45,6 +115,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
+          id="docs"
         >
           <h2 className="mb-3 text-2xl font-semibold">
             Docs{" "}
@@ -62,6 +133,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
+          id="learn"
         >
           <h2 className="mb-3 text-2xl font-semibold">
             Learn{" "}
@@ -79,6 +151,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
+          id="template"
         >
           <h2 className="mb-3 text-2xl font-semibold">
             Templates{" "}
@@ -96,6 +169,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
+          id="deploy"
         >
           <h2 className="mb-3 text-2xl font-semibold">
             Deploy{" "}
